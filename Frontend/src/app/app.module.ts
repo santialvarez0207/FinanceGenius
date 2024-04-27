592b510dd8a97cfad647256bb32cf672e1b5d85c
santialvarez0207 <68878932+santialvarez0207@users.noreply.github.com>
2024-04-27 10:04:52 -0500

    login listo

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/LoginComponent';
+import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -7,20 +7,19 @@ import { Component,OnInit } from '@angular/core';
 })
 export class LoginComponent {
 
-  
   ngOnInit():void{
 
-  const sign_in_btn:HTMLElement = document.querySelector("#sign-in-btn")?
-  const sign_up_btn:HTMLElement = document.querySelector("#sign-up-btn")?
-  const container = document.querySelector(".container");
- 
-    sign_up_btn.addEventListener("click", () =>{
-      container.classList.add("sign-up-mode");
-    });
-    
-    sign_in_btn.addEventListener("click", () =>{
-      container.classList.remove("sign-up-mode");
-    });
-}
+    const sign_in_btn:HTMLElement = document.querySelector("#sign-in-btn") as HTMLElement
+    const sign_up_btn:HTMLElement = document.querySelector("#sign-up-btn") as HTMLElement
+    const container = document.querySelector(".container") as HTMLElement
+   
+      sign_up_btn.addEventListener("click", () =>{
+        container.classList.add("sign-up-mode");
+      });
+      
+      sign_in_btn.addEventListener("click", () =>{
+        container.classList.remove("sign-up-mode");
+      });
+  }
 
 }
