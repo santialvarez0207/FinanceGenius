acc2c23ccf3da381436c81860261ee4519d521a7
brandonmartinez2002 <brandonacevedo2002@gmail.com>
2024-04-27 08:37:40 -0500

    login

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './components/login/LoginComponent';
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
