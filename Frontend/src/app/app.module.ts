1c4743e2875563089456ee5d0a04e29873765c9a
brandonmartinez2002 <brandonacevedo2002@gmail.com>
2024-05-17 19:50:54 -0500

    home_user

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { HomeUserComponent } from './components/home-user/home-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    HomeUserComponent
   ],
   imports: [
     BrowserModule,
